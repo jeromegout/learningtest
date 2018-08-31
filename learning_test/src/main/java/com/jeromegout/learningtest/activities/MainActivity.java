f3561ac188b8c7495205df91d48d2ff095ade0a5
jerome.gout <jerome.gout@gmail.com>
2018-08-31 23:26:12 +0200

    Info dialog + apk in version

--- learning_test/src/main/java/com/jeromegout/learningtest/activities/MainActivity.java
+++ learning_test/src/main/java/com/jeromegout/learningtest/activities/MainActivity.java
@@ -2,11 +2,14 @@ package com.jeromegout.learningtest.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import com.jeromegout.learningtest.EmptyRecyclerView;
 import com.jeromegout.learningtest.R;
@@ -41,7 +44,23 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(this, EditClassActivity.class);
                 startActivity(intent);
                 return true;
+            case R.id.info:
+                showAboutDialog();
+                return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void showAboutDialog() {
+        AlertDialog dialog = new AlertDialog.Builder(this)
+                .setCancelable(false)
+                .setTitle(getString(R.string.info_action_label))
+                .setIcon(null)
+                .setMessage("It's up to You\n\nJérôme Gout\n\nv20180831")
+                .setPositiveButton(getString(R.string.ok), null)
+                .show();
+        TextView messageView = dialog.findViewById(android.R.id.message);
+        assert messageView != null;
+        messageView.setGravity(Gravity.CENTER);
     }
 }
